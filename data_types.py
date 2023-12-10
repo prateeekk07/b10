60f4568864074af5873a37a8b163a7f2a5a72481
prateeekk07 <prateekc53@gmail.com>
2023-12-10 14:25:43 -0500

    My first commit

---
+++ data_types.py
@@ -0,0 +1,96 @@
+# Data types
+
+#numerical - int, Float, Complex
+
+           
+a = 5      #int    
+b = 20.5   #float
+c = 20+5j  #Complex
+
+#sequence - List, Tuple, Set, Frozenset, Range, String, Byte, Bytearray
+
+ 
+
+list = ["a","b"]                                     #list          
+tuple = ("a","b")                                    #Tuple      
+set = {10,20,30}                                     #set         
+frozenset = frozenset({10,20,30})                    #FS          
+range = range(1,11)  #1,2,3.......11                 #range          
+str = "Prateek"                                      #String
+
+#Mapping 
+
+stud_info = {"name":"Rohit", "surename":"Sharma"}    #dictonary
+
+
+#none
+
+a = None
+
+#bool
+
+flag = True
+
+
+# Rules of data types
+
+# List 
+# - []
+# - homegeneuos 
+# - hetrogeneous
+# - index based operations  -- +ve index, -ve index 
+# - support slicing
+# - Mutable -- add element, delete, update
+# - list allows mutable and im-mutable  elements
+# - list allows duplicate elements 
+
+# tuple 
+# - immutable version of list
+# - () - Paranthesis
+# - homegeneuos 
+# - hetrogeneous
+# - index based operations  -- +ve index, -ve index 
+# - support slicing
+# - Immutable
+# - tuple allows mutable and im-mutable elements
+# - tuple allows duplicate elements
+
+# str 
+# index based operations  -- +ve index, -ve index 
+# support slicing
+# immutable
+
+
+# dict
+# - {}
+# - key value pair 
+# - key can not be duplicated
+# - values can be duplicate
+# - Mutable 
+# - dict key -- immutable
+# - dict value -- mutable or immutable
+# - does not support index based operation hence slicing
+
+
+
+# string, list, dictionary
+
+
+
+# s1 = '''Pooja More
+# wvkjbcbwc skjbc kjsdc
+# ckj bewiucnwc'''
+
+# s2 = "Priya"
+# print(s1)
+
+
+
+
+# Mutable 
+# list, bytearray, dictionary, set
+
+
+
+# built in functions
+# - id, type, len
